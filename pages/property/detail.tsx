864dbf809cf0c306683d0647370f5ef437be86b4
Tony <diyorbeksolijonov27@gmail.com>
2025-10-28 08:40:24 +0900

    feat: making changes on bigpropertycard

--- pages/property/detail.tsx
+++ pages/property/detail.tsx
@@ -11,7 +11,7 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import WestIcon from '@mui/icons-material/West';
 import EastIcon from '@mui/icons-material/East';
-import { useReactiveVar } from '@apollo/client';
+import { useReactiveVar, useQuery } from '@apollo/client';
 import { useRouter } from 'next/router';
 import { Property } from '../../libs/types/property/property';
 import moment from 'moment';
@@ -25,8 +25,11 @@ import { Pagination as MuiPagination } from '@mui/material';
 import Link from 'next/link';
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { GET_PROPERTY, GET_PROPERTIES } from '../../apollo/user/query';
+import { T } from '../../libs/types/common';
 import 'swiper/css';
 import 'swiper/css/pagination';
+import { Direction } from '../../libs/enums/common.enum';
 
 SwiperCore.use([Autoplay, Navigation, Pagination]);
 
@@ -43,7 +46,7 @@ const PropertyDetail: NextPage = ({ initialComment, ...props }: any) => {
 	const [propertyId, setPropertyId] = useState<string | null>(null);
 	const [property, setProperty] = useState<Property | null>(null);
 	const [slideImage, setSlideImage] = useState<string>('');
-	const [destinationProperty, setDestinationProperty] = useState<Property[]>([]);
+	const [destinationProperties, setDestinationProperties] = useState<Property[]>([]);
 	const [commentInquiry, setCommentInquiry] = useState<CommentsInquiry>(initialComment);
 	const [propertyComments, setPropertyComments] = useState<Comment[]>([]);
 	const [commentTotal, setCommentTotal] = useState<number>(0);
@@ -54,7 +57,47 @@ const PropertyDetail: NextPage = ({ initialComment, ...props }: any) => {
 	});
 
 	/** APOLLO REQUESTS **/
+		const  {
+			loading: getPropertyLoading,
+			data: getPropertyData,
+			error: getPropertyError,
+			refetch: getPropertyRefetch,
+		} = useQuery(GET_PROPERTY, {
+			fetchPolicy: 'cache-and-network',
+			variables: { input: propertyId },
+			skip: !propertyId,
+			notifyOnNetworkStatusChange: true,
+			onCompleted: (data: T) => {
+				if(data?.getProperty) setProperty(data?.getProperty);
+				if(data?.getProperty) setSlideImage(data?.getProperty?.propertyImages[0]);
+			}
+		}); 
 
+			const  {
+				loading: getPropertiesLoading,
+				data: getPropertiesData,
+				error: getPropertiesError,
+				refetch: getPropertiesRefetch,
+			} = useQuery(GET_PROPERTIES, {
+				fetchPolicy: 'cache-and-network',
+				variables: { input: 
+					{
+						page: 1,
+						limit: 4,
+						sort: 'createdAt',
+						direction: Direction.DESC,
+						search: {
+							locationList: [property?.propertyLocation]
+						}
+					}
+				 },
+				 skip: !property && !propertyId,
+				notifyOnNetworkStatusChange: true,
+				onCompleted: (data: T) => {
+					if(data?.getProperties?.list) setDestinationProperties(data?.getProperties?.list);
+				},
+			});
+	
 	/** LIFECYCLES **/
 	useEffect(() => {
 		if (router.query.id) {
@@ -487,7 +530,7 @@ const PropertyDetail: NextPage = ({ initialComment, ...props }: any) => {
 								</Stack>
 							</Stack>
 						</Stack>
-						{destinationProperty.length !== 0 && (
+						{destinationProperties.length !== 0 && (
 							<Stack className={'similar-properties-config'}>
 								<Stack className={'title-pagination-box'}>
 									<Stack className={'title-box'}>
@@ -514,7 +557,7 @@ const PropertyDetail: NextPage = ({ initialComment, ...props }: any) => {
 											el: '.swiper-similar-pagination',
 										}}
 									>
-										{destinationProperty.map((property: Property) => {
+										{destinationProperties.map((property: Property) => {
 											return (
 												<SwiperSlide className={'similar-homes-slide'} key={property.propertyTitle}>
 													<PropertyBigCard property={property} key={property?._id} />
