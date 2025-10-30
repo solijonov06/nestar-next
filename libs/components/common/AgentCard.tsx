d2abed894f87a8f574efef172e60b7d6b0ffdf7b
Diyorbek <diyorbeksolijonov27@gmail.com>
2025-10-30 17:11:03 +0900

    feat: develop agent detail page logic

--- libs/components/common/AgentCard.tsx
+++ libs/components/common/AgentCard.tsx
@@ -12,11 +12,12 @@ import { userVar } from '../../../apollo/store';
 
 interface AgentCardProps {
 	agent: any;
+	likeMemberHandler: any;
 }
-
+  
 const AgentCard = (props: AgentCardProps) => {
-	const { agent } = props;
-	const device = useDeviceDetect();
+	const { agent, likeMemberHandler } = props;
+	const device = useDeviceDetect() ;
 	const user = useReactiveVar(userVar);
 	const imagePath: string = agent?.memberImage
 		? `${REACT_APP_API_URL}/${agent?.memberImage}`
@@ -56,15 +57,15 @@ const AgentCard = (props: AgentCardProps) => {
 							}}
 						>
 							<strong>{agent?.memberFullName ?? agent?.memberNick}</strong>
-						</Link>
+						</Link> 
 						<span>Agent</span>
 					</Box>
 					<Box component={'div'} className={'buttons'}>
-						<IconButton color={'default'}>
+						<IconButton color={'default'} onClick={() => likeMemberHandler(user, agent?._id)} >
 							<RemoveRedEyeIcon />
-						</IconButton>
+						</IconButton> 
 						<Typography className="view-cnt">{agent?.memberViews}</Typography>
-						<IconButton color={'default'}>
+						<IconButton color={'defaul t'}>
 							{agent?.meLiked && agent?.meLiked[0]?.myFavorite ? (
 								<FavoriteIcon color={'primary'} />
 							) : (
