4edc6a5507ef945ed2aa03598d36505f661a5a02
Diyorbek <diyorbeksolijonov27@gmail.com>
2025-11-04 17:14:19 +0900

    feat: develop admi related community controle

--- libs/components/admin/properties/PropertyList.tsx
+++ libs/components/admin/properties/PropertyList.tsx
@@ -155,7 +155,7 @@ export const PropertyPanelList = (props: PropertyPanelListType) => {
 									<TableRow hover key={property?._id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
 										<TableCell align="left">{property._id}</TableCell>
 										<TableCell align="left" className={'name'}>
-											<Stack direction={'row'}>
+											{property.propertyStatus === PropertyStatus.ACTIVE ? <Stack direction={'row'}>
 												<Link href={`/property/detail?id=${property?._id}`}>
 													<div>
 														<Avatar alt="Remy Sharp" src={propertyImage} sx={{ ml: '2px', mr: '10px' }} />
@@ -164,7 +164,13 @@ export const PropertyPanelList = (props: PropertyPanelListType) => {
 												<Link href={`/property/detail?id=${property?._id}`}>
 													<div>{property.propertyTitle}</div>
 												</Link>
-											</Stack>
+											</Stack> : <Stack direction={'row'}>
+													<div>
+														<Avatar alt="Remy Sharp" src={propertyImage} sx={{ ml: '2px', mr: '10px' }} />
+													</div>
+													<div style={{ marginTop: '10px' }}>{property.propertyTitle}</div>
+											</Stack>}
+											
 										</TableCell>
 										<TableCell align="center">{property.propertyPrice}</TableCell>
 										<TableCell align="center">{property.memberData?.memberNick}</TableCell>
