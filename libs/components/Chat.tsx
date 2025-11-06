5420db232571ec0e1f966fb12b66e13d039c0ed7
Diyorbek <diyorbeksolijonov27@gmail.com>
2025-11-06 14:21:29 +0900

    feat: chat.tsx

--- libs/components/Chat.tsx
+++ libs/components/Chat.tsx
@@ -7,6 +7,12 @@ import MarkChatUnreadIcon from '@mui/icons-material/MarkChatUnread';
 import { useRouter } from 'next/router';
 import ScrollableFeed from 'react-scrollable-feed';
 import { RippleBadge } from '../../scss/MaterialTheme/styled';
+import { useReactiveVar } from '@apollo/client';
+import { socketVar, userVar } from '../../apollo/store';
+import { Member } from '../types/member/member';
+import { Messages, REACT_APP_API_URL } from '../config';
+import { sweetErrorAlert } from '../sweetAlert';
+import { Message } from '@mui/icons-material';
 
 const NewMessage = (type: any) => {
 	if (type === 'right') {
@@ -32,17 +38,56 @@ const NewMessage = (type: any) => {
 	}
 };
 
+interface MessagePayLoad {
+  event: string;
+  text: string;
+  memberData: Member
+}
+
+interface InfoPayLoad {
+  event: string;
+  totalClients: number;
+  memberData: Member;
+  action: string;
+}
+
 const Chat = () => {
 	const chatContentRef = useRef<HTMLDivElement>(null);
-	const [messagesList, setMessagesList] = useState([]);
-	const [onlineUsers, setOnlineUsers] = useState<number>(4);
-	const textInput = useRef(null);
-	const [message, setMessage] = useState<string>('');
+	const [messagesList, setMessagesList] = useState<MessagePayLoad[]>([]);
+	const [onlineUsers, setOnlineUsers] = useState<number>(0);
+	const [messageInput, setMessageInput] = useState<string>('');
 	const [open, setOpen] = useState(false);
 	const [openButton, setOpenButton] = useState(false);
 	const router = useRouter();
+	const user = useReactiveVar(userVar);
+	const socket = useReactiveVar(socketVar);
+
 
 	/** LIFECYCLES **/
+
+	useEffect(() => {
+		socket.onmessage = (msg) => {
+          const data = JSON.parse(msg.data);
+		  console.log('Websocket message:', data);
+          
+		  switch (data.event) {
+			case 'info':
+				const newInfo: InfoPayLoad = data;
+				setOnlineUsers(newInfo.totalClients);
+				break;
+			case 'getMessages':
+				const list: MessagePayLoad[] = data.list;
+				setMessagesList(list);
+				break;
+			case 'message':
+				const newMessage: MessagePayLoad = data;
+				messagesList.push(newMessage);
+				setMessagesList([ ...messagesList]);
+				break;	
+		  }
+	  }
+	}, [socket, messagesList])
+	 
 	useEffect(() => {
 		const timeoutId = setTimeout(() => {
 			setOpenButton(true);
@@ -62,9 +107,9 @@ const Chat = () => {
 	const getInputMessageHandler = useCallback(
 		(e: any) => {
 			const text = e.target.value;
-			setMessage(text);
+			setMessageInput(text);
 		},
-		[message],
+		[messageInput],
 	);
 
 	const getKeyHandler = (e: any) => {
@@ -77,7 +122,13 @@ const Chat = () => {
 		}
 	};
 
-	const onClickHandler = () => {};
+	const onClickHandler = () => {
+		if (!messageInput) sweetErrorAlert(Messages.error4);
+		else {
+			socket.send(JSON.stringify({ event: 'message', data: messageInput }));
+			setMessageInput('');
+		}
+	};
 
 	return (
 		<Stack className="chatting">
@@ -100,8 +151,13 @@ const Chat = () => {
 							<Box flexDirection={'row'} style={{ display: 'flex' }} sx={{ m: '10px 0px' }} component={'div'}>
 								<div className={'welcome'}>Welcome to Live chat!</div>
 							</Box>
-							{messagesList}
-							<>
+							{messagesList.map((ele: MessagePayLoad) => {
+								const { text, memberData } = ele;
+								const memberImage = memberData?.memberImage
+								? `${REACT_APP_API_URL}/${memberData.memberImage}`
+								: '/img/profile/defaultUser.svg';
+								
+								return memberData?._id === user?._id ? (
 								<Box
 									component={'div'}
 									flexDirection={'row'}
@@ -110,22 +166,24 @@ const Chat = () => {
 									justifyContent={'flex-end'}
 									sx={{ m: '10px 0px' }}
 								>
-									<div className={'msg-right'}>hi</div>
+									<div className={'msg-right'}>{text}</div>
 								</Box>
+								) : (
 								<Box flexDirection={'row'} style={{ display: 'flex' }} sx={{ m: '10px 0px' }} component={'div'}>
-									<Avatar alt={'jonik'} src={'/img/profile/defaultUser.svg'} />
-									<div className={'msg-left'}>Hi</div>
+									<Avatar alt={'jonik'} src={memberImage} />
+									<div className={'msg-left'}>{text}</div>
 								</Box>
-							</>
+							    )
+							})}
 						</Stack>
 					</ScrollableFeed>
 				</Box>
 				<Box className={'chat-bott'} component={'div'}>
 					<input
-						ref={textInput}
 						type={'text'}
 						name={'message'}
 						className={'msg-input'}
+						value={messageInput}
 						placeholder={'Type message'}
 						onChange={getInputMessageHandler}
 						onKeyDown={getKeyHandler}
